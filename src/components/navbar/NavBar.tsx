e08758ca78096fb2812dcf5d72c2ab49f0dd6022
T-Terra <gabst138@gmail.com>
2025-04-01 14:11:38 -0300

    chore: improve navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -3,7 +3,6 @@ import {
     HomeIcon, 
     HandCoins, 
     Plus,
-    CreditCard,
     ChevronRight,
     ChevronLeft,
     List
@@ -16,12 +15,13 @@ import { motion } from "framer-motion"
 
 export default function NavBar() {
     const [open, setOpen] = useState(true)
+    const sizeIcons: number = 26
 
     return (
         <div className="w-full overflow-x-hidden bg-gray-800 flex flex-row shadow-lg">
             <motion.div
-                animate={{ marginLeft: open ? 96 : 0, marginTop: open ? 80 : 80 }} // Estado final animado
-                transition={{ duration: 0.5, ease: "backInOut" }} // Duração e efeito
+                animate={{ marginLeft: open ? 80 : 0, marginTop: open ? 80 : 80 }}
+                transition={{ duration: 0.5, ease: "backInOut" }}
                 className="absolute text-gray-100"
             >
                 <Button 
@@ -33,44 +33,37 @@ export default function NavBar() {
                 </Button>
             </motion.div>
             <motion.div
-                animate={{ padding: open ? 52 : 0, opacity: open ? 1 : 0 }} // Estado final animado
-                transition={{ duration: 0.5, ease: "backInOut" }} // Duração e efeito
+                animate={{ padding: open ? 40 : 0, opacity: open ? 1 : 0 }}
+                transition={{ duration: 0.5, ease: "backInOut" }}
                 className="bg-gray-800 text-gray-100 overflow-hidden"
             >
                 <div className="text-gray-100">
-                    <ul className="flex flex-col gap-8 items-center my-32">
+                    <ul className="flex flex-col gap-8 items-center my-35">
                         <li>
                             <Link to="/" replace>
                                 <Button type="button" style="bg-indigo-500 hover:bg-indigo-400 w-[55px] h-[55px] rounded-4xl flex justify-center items-center">
-                                    <Plus size={27}/>
+                                    <Plus size={sizeIcons}/>
                                 </Button>
                             </Link>
                         </li>
                         <li>
                             <Link to="/home" replace>
                                 <IconStyles>
-                                    <HomeIcon size={27}/>
+                                    <HomeIcon size={sizeIcons}/>
                                 </IconStyles>
                             </Link>
                         </li>
                         <li>
                             <Link to="/expenses" replace>
                                 <IconStyles>
-                                    <HandCoins size={27}/>
+                                    <HandCoins size={sizeIcons}/>
                                 </IconStyles>
                             </Link>
                         </li>
                         <li>
                             <Link to="/category" replace>
                                 <IconStyles>
-                                    <List size={27}/>
-                                </IconStyles>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/creditcard" replace>
-                                <IconStyles>
-                                    <CreditCard size={27}/>
+                                    <List size={sizeIcons}/>
                                 </IconStyles>
                             </Link>
                         </li>
