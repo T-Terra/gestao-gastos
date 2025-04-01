df76195014e20c76b75dfbee5ef0b883d35877e3
T-Terra <gabst138@gmail.com>
2025-04-01 11:23:58 -0300

    feat: add pagination table

---
+++ src/components/tables/paginatedTable.tsx
@@ -0,0 +1,84 @@
+import { useState } from "react"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { 
+    Plus,
+    Minus
+} from 'lucide-react'
+import { DataTableInterface } from "@/interfaces/DataTableInterfaces"
+
+type props = {
+    col: string[]
+    dataTable: DataTableInterface[]
+}
+
+export default function PaginatedTable({ col, dataTable }: props) {
+    const [currentPage, setCurrentPage] = useState(1)
+    const itemsPerPage: number = 10
+    const sizeIcons: number = 17
+
+    const indexOfLastItem = currentPage * itemsPerPage
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage
+    const currentItems = dataTable.slice(indexOfFirstItem, indexOfLastItem)
+
+    const totalPages = Math.ceil(dataTable.length / itemsPerPage)
+
+    const handleCell = (obj: DataTableInterface, rowIndex: number) => {
+        return (
+            <TableRow key={rowIndex} className="border-none">
+                {Object.values(obj).map((value, colIndex) => (
+                    <TableCell key={colIndex} className="font-medium">
+                        {value}
+                    </TableCell>
+                ))}
+            </TableRow>
+        )
+    }
+
+    return (
+        <div>
+            <Table>
+                <TableCaption></TableCaption>
+                <TableHeader>
+                    <TableRow className="border-none">
+                        {col.map((item) => 
+                            <TableHead className="w-[100px] text-gray-100">{item}</TableHead>
+                        )}
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {currentItems.map((data, rowIndex) => 
+                        handleCell(data, rowIndex)
+                    )}
+                </TableBody>
+            </Table>
+            {/* Botões de Paginação */}
+            <div className="flex flex-row justify-between">
+                <span className="font-semibold"> Página {currentPage} de {totalPages} </span>
+                <div className="mx-4 flex flex-row gap-1.5">
+                    <button 
+                        onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
+                        disabled={currentPage === 1}
+                        className={currentPage === 1 ? "opacity-40" : "opacity-100"}
+                    >
+                        <Minus size={sizeIcons}/>
+                    </button>
+                    <button 
+                        onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
+                        disabled={currentPage === totalPages}
+                        className={currentPage === totalPages ? "opacity-40" : "opacity-100"}
+                    >
+                        <Plus size={sizeIcons}/>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/tables/table.tsx
+++ src/components/tables/table.tsx
@@ -1,13 +1,5 @@
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
 import { DataTableInterface } from "@/interfaces/DataTableInterfaces"
+import PaginatedTable from "./paginatedTable"
 
 type props = {
     col: string[]
@@ -15,37 +7,9 @@ type props = {
 }
 
 export default function TableList({ col, dataTable }: props) {
-
-    const handleCell = (obj: DataTableInterface, rowIndex: number) => {
-        return (
-            <TableRow key={rowIndex} className="border-none">
-                {Object.values(obj).map((value, colIndex) => (
-                    <TableCell key={colIndex} className="font-medium">
-                        {value}
-                    </TableCell>
-                ))}
-            </TableRow>
-        )
-
-    }
-
     return (
         <div className="w-[1100px] text-gray-100 bg-gray-800 p-4 rounded-2xl shadow-lg">
-            <Table>
-                <TableCaption></TableCaption>
-                <TableHeader>
-                    <TableRow className="border-none">
-                        {col.map((item) => 
-                            <TableHead className="w-[100px] text-gray-100">{item}</TableHead>
-                        )}
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {dataTable.map((data, rowIndex) => 
-                        handleCell(data, rowIndex)
-                    )}
-                </TableBody>
-            </Table>
+            <PaginatedTable col={col} dataTable={dataTable} />
         </div>
     )
 }
