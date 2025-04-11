847cbf8847ff82260a14f1d9bd4baf51d9a6ba42
T-Terra <gabst138@gmail.com>
2025-04-11 11:50:43 -0300

    feat: add register axios and implements request to back

---
+++ src/interfaces/registerInterface.ts
@@ -0,0 +1,5 @@
+export interface RegisterInterface {
+    Username: string
+    Password: string
+    Email: string
+}
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,14 +1,43 @@
 import { FormRegister } from "../components/forms/formRegister"
+import { RegisterInterface } from "@/interfaces/registerInterface"
+import axios from "axios"
+import { useNavigate } from "react-router"
 
 export default function Register() {
+    const navigate = useNavigate()
+
+    const apiUrl: string = import.meta.env.VITE_API_URL
+
+    const saveRegister = async (register: RegisterInterface) => {
+        if (register.Username === "" && register.Email === "" && register.Password === "") {
+            alert("Campos vazios não foi possível salvar")
+        } else {
+            const response = await axios.post(`${apiUrl}/register`, JSON.stringify(register), {
+                headers: {"Content-Type": "application/json"}
+            })
+
+            if (response.status === 201) {
+                navigate('/')
+            }
+        }
+    }
 
     function GetFormData(formData: FormData) {
-        const name = formData.get('name')
-        const email = formData.get('email')
-        const confirmEmail = formData.get('confirmEmail')
-        const password = formData.get('password')
-        const confirmPassword = formData.get('confirmPassword')
-        
+        const name = formData.get('name') as string
+        const email = formData.get('email') as string
+        const confirmEmail = formData.get('confirmEmail') as string
+        const password = formData.get('password') as string
+        const confirmPassword = formData.get('confirmPassword') as string
+
+        if ((email === confirmEmail) && (password === confirmPassword)) {
+            const registerData: RegisterInterface = {
+                "Username": name,
+                "Password": password,
+                "Email": email
+            }
+
+            saveRegister(registerData)
+        }
     }
 
     return (
