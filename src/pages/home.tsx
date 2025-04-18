b23173213959e8fe156883efb2ae9b7c07d054f6
T-Terra <gabst138@gmail.com>
2025-04-17 23:39:41 -0300

    feat: add responsive layout home and expenses partial

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,6 +8,7 @@ import Chart from '@/components/charts/Chart'
 import User from '@/components/user/user'
 import { useEffect, useState } from 'react'
 import { formatAmount } from '@/utils/formaters'
+import Card from '@/components/card/card'
 import axios from "axios"
 
 export default function Home() {
@@ -48,75 +49,55 @@ export default function Home() {
 
     return (
         <div className="bg-gray-900 h-screen w-screen flex flex-col items-end">
-            <div className="w-[400px] py-13">
+            <div className="w-[400px] py-13 xs:ml-5">
                 <User />
             </div>
-            <div className="w-full text-gray-100 flex flex-col gap-8 items-center">
+            <div className="w-full text-gray-100 lg:flex flex-col gap-8 items-center">
                 <div  className="w-[1500px] flex justify-between">
-                    <h1 className="font-semibold text-4xl">
+                    <h1 className="font-semibold text-4xl xs:m-5">
                         Dashboard
                     </h1>
                 </div>
-                <div className="flex gap-5">
-                    <div className="h-[120px] w-[350px] bg-gray-800 rounded-4xl shadow-lg flex justify-between items-center">
-                        <div className="grid gap-1 p-5 text-2xl font-semibold">
-                            <label className="text-[15px] font-normal">Saldo Atual</label>
-                            <label>
-                                { localRevenue
-                                   ? formatAmount(handlerCurrentCost().toString())
-                                   : "R$0,00"
-                                }
-                            </label>
-                        </div>
-                        <div className="px-8">
-                            <div className="bg-blue-500 rounded-full p-2">
-                                <Banknote size={30}/>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="h-[120px] w-[350px] bg-gray-800 rounded-4xl shadow-lg flex justify-between items-center">
-                        <div className="grid gap-1 p-5 text-2xl font-semibold">
-                            <label className="text-[15px] font-normal">Receitas</label>
-                            <label>
-                                { localRevenue 
-                                    ? formatAmount(localRevenue) 
-                                    : "R$0,00"
-                                }
-                            </label>
-                        </div>
-                        <div className="px-8">
-                            <div className="bg-emerald-500 rounded-full p-2">
-                                <ChartNoAxesCombined size={30}/>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="h-[120px] w-[350px] bg-gray-800 rounded-4xl shadow-lg flex justify-between items-center">
-                        <div className="grid gap-1 p-5 text-2xl font-semibold">
-                            <label className="text-[15px] font-normal">Despesas</label>
-                            <label>                                
-                                { expensesTotal
-                                    ? `-${formatAmount(expensesTotal['total'])}`
-                                    : "R$0,00"
-                                }
-                            </label>
-                        </div>
-                        <div className="px-8">
-                            <div className="bg-red-500 rounded-full p-2">
-                                <ArrowDown size={30}/>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="h-[120px] w-[350px] bg-gray-800 rounded-4xl shadow-lg flex justify-between items-center">
-                        <div className="grid gap-1 p-5 text-2xl font-semibold">
-                            <label className="text-[15px] font-normal">Cartão de Crédito</label>
-                            <label>--</label>
-                        </div>
-                        <div className="px-8">
-                            <div className="bg-emerald-800 rounded-full p-2">
-                                <CreditCard size={30}/>
-                            </div>
-                        </div>
-                    </div>
+                <div className="lg:flex flex-wrap gap-5">
+                    <Card 
+                        label="Saldo Atual" 
+                        icon={<Banknote size={30}/>} 
+                        data={
+                            localRevenue
+                            ? formatAmount(handlerCurrentCost().toString())
+                            : "R$0,00"
+                        }
+                        styleIcon="bg-blue-500 rounded-full p-2"
+                    />
+
+                    <Card 
+                        label="Receitas" 
+                        icon={<ChartNoAxesCombined size={30}/>} 
+                        data={
+                            localRevenue
+                            ? formatAmount(localRevenue)
+                            : "R$0,00"
+                        }
+                        styleIcon="bg-emerald-500 rounded-full p-2"
+                    />
+
+                    <Card 
+                        label="Despesas" 
+                        icon={<ArrowDown size={30}/>} 
+                        data={
+                            expensesTotal
+                            ? `-${formatAmount(expensesTotal['total'])}`
+                            : "R$0,00"
+                        }
+                        styleIcon="bg-red-500 rounded-full p-2"
+                    />
+
+                    <Card 
+                        label="Cartão de Crédito" 
+                        icon={<CreditCard size={30}/>} 
+                        data="--"
+                        styleIcon="bg-emerald-800 rounded-full p-2"
+                    />
                 </div>
                 <div className="w-full text-gray-100 flex justify-center">
                     <div className="w-[700px]">
